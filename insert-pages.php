f7683adb80a9a48759a2b39fe643db70f9e66af8
figureone <figureone@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-30 19:41:51 +0000

    remove insertPages_Content id from div wrapper to allow multiple pages to be inserted; replace with insert-page and insert-page-[id] classes.
    git-svn-id: http://plugins.svn.wordpress.org/insert-pages/trunk@1123860 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- insert-pages.php
+++ insert-pages.php
@@ -214,7 +214,7 @@ if ( !class_exists( 'InsertPagesPlugin' ) ) {
 			$wp_query = clone $temp_query; // Restore main Loop's wp_query
 			$post = $temp_post;
 
-			$content = "<div data-post-id='$page' id='insertPages_Content'>$content</div>";
+			$content = "<div data-post-id='$page' class='insert-page insert-page-$page'>$content</div>";
 			return $content;
 			//return do_shortcode($content); // careful: watch for infinite loops with nested inserts
 		}
