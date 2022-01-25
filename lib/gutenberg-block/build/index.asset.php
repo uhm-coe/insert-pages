ecec99fae260dfc540d53b1890166b5613743c76
Paul Ryan <paul.rar@gmail.com>
2022-01-25 10:21:03 -1000

    Build block

--- lib/gutenberg-block/build/index.asset.php
+++ lib/gutenberg-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-server-side-render', 'wp-url'), 'version' => '7812965476ba29090a64b7fac43bcd80');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'e6ed0514afdfb8764fd09e92a1a7f2ba');
