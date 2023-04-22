ee6ea18fcda77bb01a733bfae17d75879d93c12c
Paul Ryan <paul.rar@gmail.com>
2023-04-21 15:39:46 -1000

    Tested up to WordPress 6.2

--- lib/gutenberg-block/build/index.asset.php
+++ lib/gutenberg-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '07f66a818b262881a522');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '9c7de9252a4dcebfde63');
--- readme.txt
+++ readme.txt
@@ -1,7 +1,7 @@
 === Insert Pages ===
 Contributors: figureone, the_magician
 Tags: insert, pages, shortcode, embed
-Tested up to: 6.1
+Tested up to: 6.2
 Stable tag: trunk
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
