c5f0a79a586e596020f93a8fc1dd18c0f496f1ed
Paul Ryan <paul.rar@gmail.com>
2023-04-21 15:43:52 -1000

    Rebuild gutenberg block

--- lib/gutenberg-block/build/index.asset.php
+++ lib/gutenberg-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '9c7de9252a4dcebfde63');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '6369fa60b0dabae2f726');
