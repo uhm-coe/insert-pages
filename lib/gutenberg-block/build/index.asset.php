9851b1f070fb0bfe76d94d169d0ad29a12450e55
Paul Ryan <paul.rar@gmail.com>
2024-04-16 14:37:50 -1000

    Build block

--- lib/gutenberg-block/build/index.asset.php
+++ lib/gutenberg-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '6369fa60b0dabae2f726');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '31114ffc683b300f3935');
